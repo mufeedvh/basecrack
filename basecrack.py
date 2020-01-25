8b432fd11dc76e2b26fa90e257951b8b8e60dc40
Tibin Sunny <33937416+Tibinsunny@users.noreply.github.com>
2020-01-25 19:23:59 +0530

    Version handling
    I think this is the better way to handle the version checking .
    Google said try except is much slower than an  if condition  :)

--- basecrack.py
+++ basecrack.py
@@ -8,18 +8,17 @@ __github__  = "https://github.com/mufeedvh"
 
 # importing some required stuff
 import re
-import argparse
+import sys
 from termcolor import colored
-import base36
-import base58
-import base62
-import base64
-import base91
-
-# compatibility handling
-try:
+if sys.version_info[0] < 3:
+    import argparse
+    import base36
+    import base58
+    import base62
+    import base64
+    import base91
     import base92
-except ImportError:
+else:
     print(colored("\nBaseCrack is not compatible with Python3, use Python2.\n", "red"))
     quit()
 
